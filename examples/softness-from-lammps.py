d796fa169e1959b48626f7256d33c954d6efeab7
Ian Graham <irgraham1@gmail.com>
2022-09-26 18:59:51 -0400

    phop working for lammpstrj

--- examples/softness-from-lammps.py
+++ examples/softness-from-lammps.py
@@ -3,6 +3,10 @@ import MDAnalysis.topology
 import MDAnalysis.coordinates
 import mdtraj
 import freud
+import time
+import numpy as np
+
+from schmeud._schmeud import dynamics, ml
 
 # reader = MDAnalysis.coordinates.DCD.DCDReader('trajectory.dcd')
 # u = mda.Universe('input.data', 'input.dump', atom_style='id type mol x y z')
@@ -10,11 +14,21 @@ import freud
 # parse = MDAnalysis.topology.LAMMPSParser.DATAParser('input.data')
 # top = parse.parse()
 # print(top.n_atoms)
+now = time.time()
 u = MDAnalysis.coordinates.LAMMPS.DumpReader('input.dump')
 print(u.n_frames)
-for ts in u.trajectory:
-    print(ts.positions[40:42])
-    break
+data = []
+for ts in u.trajectory[:1000]:
+    data.append(ts.positions)
+data = np.array(data).astype(np.float32)
+print(time.time() - now)
+print(data.shape)
+now = time.time()
+phop = dynamics.p_hop(data, 101)
+print(time.time() - now)
+
+# print(phop)
+
 # t = mdtraj.load_lammpstrj('input.dump', top='input_trj.psf', frame=0)
 
 # print(t.n_frames)
