648facec04be7f01530eeed3c2b894e5b3be4542
Ian Graham <irgraham1@gmail.com>
2022-09-26 18:42:29 -0400

    can finally read lammpsdump files

--- examples/softness-from-lammps.py
+++ examples/softness-from-lammps.py
@@ -1,10 +1,21 @@
 import MDAnalysis as mda
+import MDAnalysis.topology
+import MDAnalysis.coordinates
 import mdtraj
 import freud
 
 # reader = MDAnalysis.coordinates.DCD.DCDReader('trajectory.dcd')
-u = mda.Universe('input.data', 'input.dump', atom_style='id mol x y z')
-# t = mdtraj.load_lammpstrj('input.dump', top='input_trj.pdb')
+# u = mda.Universe('input.data', 'input.dump', atom_style='id type mol x y z')
+
+# parse = MDAnalysis.topology.LAMMPSParser.DATAParser('input.data')
+# top = parse.parse()
+# print(top.n_atoms)
+u = MDAnalysis.coordinates.LAMMPS.DumpReader('input.dump')
+print(u.n_frames)
+for ts in u.trajectory:
+    print(ts.positions[40:42])
+    break
+# t = mdtraj.load_lammpstrj('input.dump', top='input_trj.psf', frame=0)
 
 # print(t.n_frames)
 # rdf = freud.density.RDF(100, 4.0)
