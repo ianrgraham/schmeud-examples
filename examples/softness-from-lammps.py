2b792bd1720af2ccd5480fba21c0a55d1935a9f0
Ian Graham <irgraham1@gmail.com>
2022-09-26 17:58:43 -0400

    update

--- examples/softness-from-lammps.py
+++ examples/softness-from-lammps.py
@@ -1,9 +1,13 @@
-import MDAnalysis
+import MDAnalysis as mda
+import mdtraj
 import freud
 
-reader = MDAnalysis.coordinates.DCD.DCDReader('trajectory.dcd')
+# reader = MDAnalysis.coordinates.DCD.DCDReader('trajectory.dcd')
+u = mda.Universe('input.data', 'input.dump', atom_style='id mol x y z')
+# t = mdtraj.load_lammpstrj('input.dump', top='input_trj.pdb')
 
-rdf = freud.density.RDF(100, 4.0)
+# print(t.n_frames)
+# rdf = freud.density.RDF(100, 4.0)
 
-for frame in reader:
-    rdf.compute(system=frame, reset=False)
+# for frame in reader:
+#     rdf.compute(system=frame, reset=False)
